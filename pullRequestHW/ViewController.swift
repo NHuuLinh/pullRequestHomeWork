77624f3f2555535d473dfc052fc3500c4b226f05
LinhMAC <nguyenlinh@LinhMACs-MacBook-Pro.local>
2023-08-13 21:38:37 +0700

    feature/make-tutorial

--- pullRequestHW/ViewController.swift
+++ pullRequestHW/ViewController.swift
@@ -3,13 +3,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         testPullRequest()
-        
+        check()
     }
     func testPullRequest(){
         print("testing")
     }
-}
+    func check() {
+        let i = Int.random(in: 1...10)
+        if i % 2 == 0 {
+                print("true")
+            } else {
+                print("false")
+            }
+        print("\(i)")
+            
+        }
+    }
+
